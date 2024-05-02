7e60af5e5529e0a24f94f21a326f9fc774a90c86
CRFL0online <chris.rod8764@gmail.com>
2024-05-01 20:39:23 -0400

    push to main

--- secureAuthentication/credentialMaker.py
+++ secureAuthentication/credentialMaker.py
@@ -1,13 +1,17 @@
 from faker import Faker
 import secrets as secret
-
+import hashlib
 faker = Faker()
 file = open("credentials.txt", 'a')
+hashes = open("hash_passwords.txt",'a')
 
 for a in range(10):
-    u_name = faker.name()
-    u_name = (u_name.replace(" ","")).lower()
-    password = secret.token_urlsafe(15)
-    file.write(u_name + "," + password + "\n")
+    username = faker.name()
+    username = (username.replace(" ","")).lower()
+    password = secret.token_urlsafe(16)
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    file.write(f"{username},{password}\n")
+    hashes.write(f"{username},{hashed_password}\n")
 
 file.close()
+hashes.close()
--- secureAuthentication/server.py
+++ secureAuthentication/server.py
@@ -1,11 +1,60 @@
 import socket
-import ssl
+import threading
+import random
+import hashlib
 
-#define function to initialize socket connection
-def server_start():
+
+HOST = "127.0.0.1"
+port = 54321
+g= 423
+p = 1299827
+
+
+def start_dh(client_socket):
+    client_socket.send(str(p).encode("utf-8"))
+    client_socket.send(str(g).encode("utf-8"))
+
+    private_key = random.randrange(p-1)
+    public_key = (g ** private_key) % p
+
+    client_socket.send(str(public_key).encode("utf-8"))
+    client_key = client_socket.recv(1024).decode("utf-8")
+    client_key = int(client_key)
+
+    shared_key = (client_key ** private_key) % p
+    client_socket.send(str(shared_key).encode("utf-8"))
+    peer_shared_key = client_socket.recv(1024).decode("utf-8")
+    peer_shared_key = int(peer_shared_key)
+
+    if (shared_key == peer_shared_key):
+        key = hashlib.sha256(shared_key.to_bytes(32, byteorder = 'big')).hexdigest()
+        return (key, True)
+    else:
+        return (None, False)
+
+# define function to initialize socket connection
+def server_loop():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((HOST, port))
+        print("Socket successfully created")
+    except socket.error as err:
+        print("socket creation failed with error %s" % err)
+        return
+    server.listen(5)
+    print("Server accepting connections\n")
+
+    while True :
+        client_socket, addr = server.accept()
+        print(f"Accepted Connection from client address {addr}")
+        client_thread = threading.Thread(target=main_client, args=(client_socket, addr))
+        client_thread.start()
+
+
+def main_client(client_socket, addr):
+    """
     HOST = "127.0.0.1"
     port = 54321
-
 
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_verify_locations('selfsigned.crt')
@@ -24,52 +73,76 @@ def server_start():
 
     print("Server accepting connections\n")
     print(f"Accepted Connection from client address {addr}")
-    secure_server = context.wrap_socket(client_socket, server_side=True)
+    #secure_server = context.wrap_socket(client_socket, server_side=True)
+    """
+    key, authenticated = start_dh(client_socket)
+    if not authenticated:
+        print("Issue authenticating")
+        return 0
+    print("Client Authenticated")
 
-    #secure_server = context.wrap_socket(server, server_side=True)
     while True:
-    
         try:
             client_socket.send("Insert Username:".encode("utf-8"))
             username = client_socket.recv(1024).decode("utf-8")
             client_socket.send("Insert Password:".encode("utf-8"))
             password = client_socket.recv(1024).decode("utf-8")
-            if (check_credentials(username,password)):
+            if check_credentials(username, password):
                 print("Client connected")
                 client_socket.send("Login successful".encode("utf-8"))
                 handle_client(client_socket, addr)
                 break
             else:
                 client_socket.send("Login failure".encode("utf-8"))
-                print(f"Client connection at address {addr} closed")
-                break
-
+                register = client_socket.recv(1024).decode("utf-8")
+                if register == 'y':
+                    new_username = client_socket.recv(1024).decode("utf-8")
+                    new_password = client_socket.recv(1024).decode("utf-8")
+                    add_user(new_username, new_password)
+                else:
+                    print(f"Client connection at address {addr} closed")
+                    break
         except Exception as e:
-                print(f"Error handling connection from {addr}: {e}")
-
-        break
+            print(f"Error handling connection from {addr}: {e}")
 
     client_socket.close()
-    secure_server.close()
     print("Server Closed")
 
     return 0
-    
-#def function that checks for the user credential (no hash yet)
+
+
+def add_user(username, password):
+    file = open("credentials.txt", "a")
+    hashes = open("hash_passwords.txt", "a")
+
+    file.write(f"{username},{password}\n")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    hashes.write(f"{username},{hashed_password}\n")
+
+    file.close()
+    hashes.close()
+
+    return 0
+
+# def function that checks for the user credential
 def check_credentials(username, password):
-    with open("credentials.txt", "r") as file:
+    with open("hash_passwords.txt", "r") as file:
         for line in file:
             stored_username, stored_password = line.strip().split(',')
+            password = hashlib.sha256(password.encode()).hexdigest()
             if stored_username == username and stored_password == password:
-                return True 
+                file.close()
+                return True
+        file.close()
     return False
+
 
 def handle_client(client_socket, addr):
 
     while True:
         response = client_socket.recv(1024)
         response = response.decode("utf-8")
-        if (response.lower == "close"):
+        if response.lower == "close":
             client_socket.close()
             print(f"Client connection at address {addr} closed")
             return 0
@@ -78,7 +151,6 @@ def handle_client(client_socket, addr):
             data = "Message Received".encode("utf-8")
             client_socket.send(data)
 
-        
 
 if __name__ == '__main__':
-    server_start()
+    server_loop()
